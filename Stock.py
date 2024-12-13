f8788ff67927032dc2ca4cc62866fe60f140ab56
Erik Berglund <erik.berglund2@elev.ga.ntig.se>
2024-12-13 14:27:45 +0100

    Gjort en class för att hämta stocks

--- Stock.py
+++ Stock.py
@@ -1,8 +1,28 @@
 import yfinance as yf
+from Portfolio import Portfolio
 
-ticker = input("Input symbol (default AAPL): ") or "AAPL"
 
-stock = yf.Ticker(ticker)
-history = stock.history(period="1d")["Close"].iloc[-1]
+class Stock(Portfolio):
+    def __init__(self, ticker, quantity):
+        self.ticker = ticker
+        self._quantity = quantity
+        
+        super().__init__(price=0, market_cap=0)
+        self._price, self._market_cap = self.fetch_stock_data()
 
-print(f"The latest closing price of {ticker} is: {history}")
+    def fetch_stock_data(self):
+        stock = yf.Ticker(self.ticker)
+        stock_info = stock.info
+
+        price = stock_info.get("currentPrice")
+        market_cap = stock_info.get("marketCap")
+
+        return price, market_cap
+
+    def get_value(self):
+        return self._price * self._quantity
+
+    def __str__(self):
+        return (super().__str__() +
+                f", Ticker: {self.ticker}, Quantity: {self._quantity}, "
+                f"Total Value: {self.get_value()}")
