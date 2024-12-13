835ec0ec7b100c9231094e50f96d96a04d5fcabf
Erik Berglund <erik.berglund2@elev.ga.ntig.se>
2024-12-13 14:30:51 +0100

    Gjorde så att Crypto klassen extrendar Portfolio klassen

--- Crypto.py
+++ Crypto.py
@@ -1,17 +1,19 @@
 import requests
+import Portfolio
 
-
-class Crypto:
+class Crypto(Portfolio):
     def __init__(self, price, market_cap, current_supply, volume24, total_supply):
-        self._price = price
-        self._market_cap = market_cap
+        super().__init__(price, market_cap)
         self._current_supply = current_supply
         self._volume24 = volume24
         self._total_supply = total_supply
 
+    def get_current_supply(self):
+        return self._price * self._current_supply
+
     def __str__(self):
-        return (f"Price: {self._price}, Market Cap: {self._market_cap}, "
-                f"Current Supply: {self._current_supply}, Volume (24h): {self._volume24}, "
+        return (super().__str__() +
+                f", Current Supply: {self._current_supply}, Volume (24h): {self._volume24}, "
                 f"Total Supply: {self._total_supply}")
 
     def run_crypto():
---
+++ Portfolio.py
@@ -0,0 +1,4 @@
+class Portfolio:
+    def __init__(self, price, market_cap=None):
+        self._price = price
+        self._market_cap = market_cap
